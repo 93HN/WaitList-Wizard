f712ad74c0aeb4ea50f2aac4f8f4fb3c1398fad3
Fiasco <horaciomota93@gmail.com>
2024-06-12 23:49:24 +0100

    feat: Create Project model

---
+++ src/main/java/com/horaceneto/WaitListWizard/model/Project.java
@@ -0,0 +1,25 @@
+package com.horaceneto.WaitListWizard.model;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "project")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Project {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private Boolean active;
+
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+
+}
