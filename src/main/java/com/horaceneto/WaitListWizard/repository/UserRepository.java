6754e620d640214d58ec2142d9775803e36c97a9
Fiasco <horaciomota93@gmail.com>
2024-06-13 00:11:34 +0100

    feat: create UserRepository

---
+++ src/main/java/com/horaceneto/WaitListWizard/repository/UserRepository.java
@@ -0,0 +1,6 @@
+package com.horaceneto.WaitListWizard.repository;
+import com.horaceneto.WaitListWizard.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+}
