25b1b07bccb49c281056d6710fe66ae79e68ff27
Fiasco <horaciomota93@gmail.com>
2024-06-13 00:11:43 +0100

    feat: create userDTO

---
+++ src/main/java/com/horaceneto/WaitListWizard/dto/UserDTO.java
@@ -0,0 +1,18 @@
+package com.horaceneto.WaitListWizard.dto;
+import java.time.LocalDateTime;
+
+public class UserDTO {
+
+    private Long id;
+
+    private String name;
+
+    private String email;
+
+    private String password;
+
+    private Boolean active;
+
+    private LocalDateTime createdAt;
+
+}
