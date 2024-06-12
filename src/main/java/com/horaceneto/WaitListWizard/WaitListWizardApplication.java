c68a221e58cb6f3a0c36bf8441563132804a3b1e
Fiasco <horaciomota93@gmail.com>
2024-06-12 22:35:09 +0100

    first commit

---
+++ src/main/java/com/horaceneto/WaitListWizard/WaitListWizardApplication.java
@@ -0,0 +1,13 @@
+package com.horaceneto.WaitListWizard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WaitListWizardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WaitListWizardApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/horaceneto/WaitListWizard/WaitListWizardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.horaceneto.WaitListWizard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WaitListWizardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
